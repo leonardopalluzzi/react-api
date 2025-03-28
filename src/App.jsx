2f659f1d12db29a5492bb709d696ee1e93cbd5aa
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-03-28 15:23:06 +0100

    feat: implement data fetching and display in a responsive table

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,15 @@ const endpoint = 'http://localhost:3000/api/v1/posts'
 
 function App() {
 
+  const [data, setData] = useState([])
+
 
   function fetchData(url) {
     fetch(url)
       .then(res => res.json())
       .then(data => {
         console.log(data);
-
+        setData(data)
       })
   }
 
@@ -18,9 +20,39 @@ function App() {
     fetchData(endpoint)
   }, [])
 
-
   return (
     <>
+      <div
+        className="table-responsive"
+      >
+        <table
+          className="table table-primary"
+        >
+          <thead>
+            <tr className="">
+              <td scope="row">Title</td>
+              <td>Slug</td>
+              <td>Content</td>
+              <td>IMG</td>
+              <td>TAGS</td>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map(post => {
+              <tr key={post.slug} className="">
+                <td scope="row">{post.title}</td>
+                <td>{post.slug}</td>
+                <td>{post.content}</td>
+                <td>{post.image}</td>
+                <td>{post.tags}</td>
+              </tr>
+            })}
+
+
+          </tbody>
+        </table>
+      </div>
+
 
     </>
   )
